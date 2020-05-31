7a7d2bea8055121838b3dfe860caf902a97d0aaa
Anil Rayamajhi <gi@Anils-MacBook-Pro.local>
2020-05-31 14:25:18 -0700

    assignment 1.5 - typesValuesVariables

---
+++ week-1/rayamajhi-assignment1.5-typesValuesVariables.js
@@ -0,0 +1,68 @@
+var personOneFirstName;
+personOneFirstName = "Cody";
+console.log(personOneFirstName);
+
+var personOneLastName;
+personOneLastName = "Gunn";
+console.log(personOneLastName);
+
+var personOneAddress;
+personOneAddress = "1650 Stradella Rd, Los Angeles, 90077-8262";
+console.log(personOneAddress);
+
+var personOnePayRate;
+personOnePayRate = 24.324;
+console.log(personOnePayRate.toFixed(1));
+
+var personOneHireDate;
+personOneHireDate = "2017-3-31";
+personOneFormattedHireDate = new Date(personOneHireDate);
+console.log(personOneFormattedHireDate.toLocaleDateString());
+
+console.log("\n*************************");
+
+var personTwoFirstName;
+personTwoFirstName = "Liam";
+console.log(personTwoFirstName);
+
+var personTwoLastName;
+personTwoLastName = "Downey";
+console.log(personTwoLastName);
+
+var personTwoAddress;
+personTwoAddress = "1650 Beverly Glen, Los Angeles, 90023";
+console.log(personTwoAddress);
+
+var personTwoPayRate;
+personTwoPayRate = 44.31314;
+console.log(personTwoPayRate.toFixed(1));
+
+var personTwoHireDate;
+personTwoHireDate = "2019-5-1";
+personTwoFormattedHireDate = new Date(personTwoHireDate);
+console.log(personTwoFormattedHireDate.toLocaleDateString());
+
+console.log("\n*************************");
+
+var personThreeFirstName;
+personThreeFirstName = "Tmethy";
+console.log(personThreeFirstName);
+
+var personThreeLastName;
+personThreeLastName = "Pane";
+console.log(personThreeLastName);
+
+var personThreeAddress;
+personThreeAddress = "4352 Swattle blvd, Oxnard, 98374";
+console.log(personThreeAddress);
+
+var personThreePayRate;
+personThreePayRate = 9.31314;
+console.log(personThreePayRate.toFixed(1));
+
+var personThreeHireDate;
+personThreeHireDate = "2020-1-11";
+personThreeFormattedHireDate = new Date(personThreeHireDate);
+console.log(personThreeFormattedHireDate.toLocaleDateString());
+
+console.log("\n*************************\n");
