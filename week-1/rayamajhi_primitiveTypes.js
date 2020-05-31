730fb5b5186767e579d47bb0b4dc189cc40c5ec7
Anil Rayamajhi <gi@Anils-MacBook-Pro.local>
2020-05-31 13:45:12 -0700

    Assignment 1.4 - Primitive Types

---
+++ week-1/rayamajhi_primitiveTypes.js
@@ -0,0 +1,23 @@
+var firstName;
+firstName = "Anil";
+console.log(firstName);
+
+var lastName;
+lastName = "Rayamajhi";
+console.log(lastName);
+
+var age;
+age = 29;
+console.log(age);
+
+var street;
+street = "1650 Stradella Rd";
+console.log(street);
+
+var city;
+city = "Los Angeles";
+console.log(city);
+
+var zipCode;
+zipCode = "90077-8266";
+console.log(zipCode);
