1c23864259c5072e2f61c9b15255c4304218d37f
Anil Rayamajhi <gi@Anils-MacBook-Pro.local>
2020-06-07 13:54:25 -0700

    week 2 > Exercise 2.3

---
+++ week-2/finished-code/exercise-2.3.js
@@ -0,0 +1,30 @@
+/*
+  Expected output:
+
+  Anil Rayamajhi
+  Exercise 2.3
+  June 7, 2020
+
+  Deliverable:
+  Hello FirstName LastName!
+
+  Hint: Use your personal header display function and
+  refer to page 178 for implementing function properties
+
+  Execute: `node week-2/finished-code/exercise-2.3.js`
+*/
+
+// function property
+myName.anil = "Anil";
+
+/**
+ * Return: string
+ *
+ * Response: Function Prototype "anil"
+ * Description: Returns a well-formatted string header
+ */
+function myName() {
+  return myName.anil;
+}
+
+console.log(`\nHello ${myName()} Rayamajhi!`);
--- week-2/rayamajhi-header-test.js
+++ week-2/rayamajhi-header-test.js
@@ -1,9 +1,11 @@
 /*
 ============================================
 ; Title:  header-test.js
-; Author: Professor Cross 
+; Author: Professor Cross
 ; Date:   07 June 2020
 ; Description: Displays a formatted header
+;
+; Execute: `node week-2/rayamajhi-header-test.js`
 ;===========================================
 */
 
