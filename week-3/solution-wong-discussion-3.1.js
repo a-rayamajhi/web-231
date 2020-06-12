130664d81adb786bd030bb50ed053ef3624fec39
Anil Rayamajhi <gi@Anils-MacBook-Pro.local>
2020-06-11 23:02:28 -0700

    discussion 3.1

---
+++ week-3/solution-wong-discussion-3.1.js
@@ -0,0 +1,18 @@
+/*
+============================================
+; Title:  control statements.js
+; Author:  Devan Wong
+; Date:   11 June 2020
+; Modified By: Anil Rayamajhi
+; Description: 2 Errors please fix.
+;===========================================
+*/
+
+//corrected instance variable and semi colon
+for (let i = 0; i <= 10; i++) {
+  if (i == 5) {
+    console.log("this hit five");
+  } else {
+    console.log("i = " + i);
+  }
+}
